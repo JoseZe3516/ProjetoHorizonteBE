06c0163051bf97f5acd689c866eafd8dc6d3f796
Leonardo Lopes Lessa da Fonseca <leonardo.lessa@materacapital.com.br>
2022-12-04 22:24:32 -0300

    Inserido ID do Projeto

--- src/sistema_arquivos.py
+++ src/sistema_arquivos.py
@@ -1,18 +1,23 @@
 import os
 
 class FileSystem:
-    def __init__(self, cpf: str):
+    def __init__(self, cpf: str, project_id: int):
         self.cpf: cpf = cpf
+        self.project_id = project_id
         self.path: str = './src/data/'
-        self.folder_path: str = os.path.join(self.path, self.cpf)
+        self.user_folder_path: str = os.path.join(self.path, self.cpf)
+        self.project_folder_path: str = os.path.join(self.user_folder_path, self.project_id)        
 
     def create_file(self, file_name: str, file: bytes) -> bool: 
 
-        if not os.path.exists(self.folder_path):
-            os.mkdir(self.folder_path)
+        if not os.path.exists(self.user_folder_path):
+            os.mkdir(self.user_folder_path)
+
+        if not os.path.exists(self.project_folder_path):
+            os.mkdir(self.project_folder_path)    
 
         try:
-            with open(os.path.join(self.folder_path, file_name), 'xb') as f:
+            with open(os.path.join(self.project_folder_path, file_name), 'xb') as f:
                 f.write(file)
         except Exception as e:
             return False
@@ -21,7 +26,7 @@ class FileSystem:
     
     def read_file(self, file_name: str) -> bytes:
 
-        file_path = os.path.join(self.folder_path, file_name)
+        file_path = os.path.join(self.project_folder_path, file_name)
 
         if not os.path.isfile(file_path): return None
 
@@ -35,13 +40,13 @@ class FileSystem:
     
     def update_file(self, file_name: str, file: bytes) -> bool:
 
-        file_path = os.path.join(self.folder_path, file_name)
+        file_path = os.path.join(self.project_folder_path, file_name)
 
         if not os.path.isfile(file_path): return False
-        if not os.path.exists(self.folder_path): return False
+        if not os.path.exists(self.project_folder_path): return False
 
         try:
-            with open(os.path.join(self.folder_path, file_name), 'wb') as f:
+            with open(os.path.join(self.project_folder_path, file_name), 'wb') as f:
                 f.write(file)
         except Exception as e:
             return False
@@ -49,10 +54,10 @@ class FileSystem:
         return True
     
     def delete_file(self, file_name: str) -> bool:
-        file_path = os.path.join(self.folder_path, file_name)
+        file_path = os.path.join(self.project_folder_path, file_name)
 
         if not os.path.isfile(file_path): return False
-        if not os.path.exists(self.folder_path): return False
+        if not os.path.exists(self.project_folder_path): return False
 
         try:
             os.remove(file_path)
@@ -63,6 +68,6 @@ class FileSystem:
     
     def list_files(self) -> list:        
 
-        if not os.path.exists(self.folder_path): return None
+        if not os.path.exists(self.project_folder_path): return None
 
-        return os.listdir(self.folder_path)
+        return os.listdir(self.project_folder_path)
